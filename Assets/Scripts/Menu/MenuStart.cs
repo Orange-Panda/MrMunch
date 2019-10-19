23b14916f7bbcce63a7f14a197aa42b747475b49
Luke Mirman <OrangeJuicePanda1@gmail.com>
2019-10-19 14:41:53 -0400

    Menu and transition

---
+++ Assets/Scripts/Menu/MenuStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStart : MonoBehaviour
+{
+	public string sceneToLoad;
+	private bool transitioning = false;
+
+	void Update()
+    {
+        if (Input.anyKeyDown && Time.timeSinceLevelLoad > 2f && transitioning == false)
+		{
+			transitioning = true;
+			StartCoroutine(ChangeScene());
+		}
+    }
+
+	private IEnumerator ChangeScene()
+	{
+		Instantiate(Resources.Load<GameObject>("Scene Transition"));
+		yield return new WaitForSeconds(1f);
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
---
+++ Assets/Scripts/Menu/MenuTransition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTransition : MonoBehaviour
+{
+	private Animator animator;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+		DontDestroyOnLoad(gameObject);
+		Destroy(gameObject, 10f);
+	}
+
+	private void OnEnable()
+	{
+		SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
+	}
+
+	private void OnDisable()
+	{
+		SceneManager.activeSceneChanged -= SceneManager_activeSceneChanged;
+	}
+
+	private void SceneManager_activeSceneChanged(Scene arg0, Scene arg1)
+	{
+		animator.Play("TransitionEnd", 0, 0f);
+	}
+}
