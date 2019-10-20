ef48d843739174a73447bafe25560b79ccee7c14
Luke Mirman <OrangeJuicePanda1@gmail.com>
2019-10-20 12:52:05 -0400

    HUD

---
+++ Assets/Scripts/HUD/HUDClock.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDClock : MonoBehaviour
+{
+	public Image clockBack;
+	public Image clockFill;
+	public Image clockHand;
+
+	private Timer timer;
+
+    void Start()
+    {
+		InvokeRepeating("CheckForTimer", 0f, 1f);
+    }
+
+	private void CheckForTimer()
+	{
+		if (timer == null)
+		{
+			timer = FindObjectOfType<Timer>();
+		}
+	}
+
+    void Update()
+    {
+		if (timer == null)
+		{
+			clockBack.enabled = false;
+			clockFill.enabled = false;
+			clockHand.enabled = false;
+		}
+		else
+		{
+			clockBack.enabled = true;
+			clockFill.enabled = true;
+			clockHand.enabled = true;
+		}
+
+		float progress = timer.TimeRemaining / timer.StartTime; //0-end 1-start
+		clockHand.rectTransform.rotation = Quaternion.Euler(0, 0, -360 * progress);  //0-360
+		clockFill.fillAmount = progress;
+    }
+}
